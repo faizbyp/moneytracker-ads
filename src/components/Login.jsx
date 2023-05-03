41f177b6ac859f47c5e18904d7c67149e3409255
faizbyp <faizbyp@gmail.com>
2023-05-03 17:56:36 +0700

    installing bootstrap

---
+++ src/components/Login.jsx
@@ -0,0 +1,26 @@
+function Login() {
+  return (
+    <div className="d-flex justify-content-center align-items-center bg-green vh-100">
+      <main className="bg-white rounded-5 w-75">
+        <h1 className="my-4 text-center fw-bold">Log In</h1>
+        <form className="mx-5">
+          <div className="mb-3">
+            <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+            <div id="emailHelp" className="form-text">Well never share your email with anyone else.</div>
+          </div>
+          <div className="mb-3">
+            <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+            <input type="password" className="form-control" id="exampleInputPassword1" />
+          </div>
+          <div className="mb-3 form-check">
+            <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+            <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+          </div>
+          <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
+      </main>
+    </div>
+  )
+}
+export default Login
