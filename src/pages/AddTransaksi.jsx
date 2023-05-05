d3e4d09f54e7ef394516c97ee387a0c512f982f3
faizbyp <faizbyp@gmail.com>
2023-05-05 23:43:21 +0700

    done slicing page tambah

--- src/pages/AddTransaksi.jsx
+++ src/pages/AddTransaksi.jsx
@@ -11,22 +11,94 @@ function AddTransaksi() {
         <h1 className="m-0">Tambah Transaksi</h1>
       </header>
 
-      <ul className="nav nav-tabs nav-fill" id="myTab" role="tablist">
-        <li className="nav-item" role="presentation">
-          <button className="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#pendapatan-tab-pane" type="button" role="tab" aria-controls="pendapatan-tab-pane" aria-selected="true">Pendapatan</button>
-        </li>
-        <li className="nav-item" role="presentation">
-          <button className="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#pengeluaran-tab-pane" type="button" role="tab" aria-controls="pengeluaran-tab-pane" aria-selected="false">Profile</button>
-        </li>
-      </ul>
-      <div className="tab-content" id="myTabContent">
-        <div className="tab-pane fade show active" id="pendapatan-tab-pane" role="tabpanel" aria-labelledby="pendapatan-tab" tabIndex="0">
-          <h3 className="py-3 text-center">Tab Pendapatan</h3>
+      <main className="bg-white rounded-5 p-3 mb-4">
+
+        <ul className="nav nav-tabs nav-fill" id="myTab" role="tablist">
+          <li className="nav-item" role="presentation">
+            <button className="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#pendapatan-tab-pane" type="button" role="tab" aria-controls="pendapatan-tab-pane" aria-selected="true">Pendapatan</button>
+          </li>
+          <li className="nav-item" role="presentation">
+            <button className="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#pengeluaran-tab-pane" type="button" role="tab" aria-controls="pengeluaran-tab-pane" aria-selected="false">Pengeluaran</button>
+          </li>
+        </ul>
+
+        <div className="tab-content" id="myTabContent">
+
+          <div className="tab-pane fade show active" id="pendapatan-tab-pane" role="tabpanel" aria-labelledby="pendapatan-tab" tabIndex="0">
+            <form className="my-4 text-gray-50">
+              <div className="d-flex align-items-center mb-3">
+                <label className="form-label d-inline m-0 me-2">Tanggal</label>
+                <input name="tanggal" type="date" className="form-control border-0 border-bottom bg-transparent shadow-none" />
+              </div>
+              <div className="d-flex align-items-center mb-3">
+                <label className="form-label d-inline m-0 me-2">Jumlah</label>
+                <input name="jumlah" type="text" className="form-control border-0 border-bottom bg-transparent shadow-none" />
+              </div>
+              <div className="d-flex align-items-center mb-3">
+                <label className="form-label d-inline m-0 me-2">Kategori</label>
+                <select className="form-select border-0 border-bottom bg-transparent shadow-none" aria-label="Default select example">
+                  <option selected />
+                  <option value="1">One</option>
+                  <option value="2">Two</option>
+                  <option value="3">Three</option>
+                </select>
+              </div>
+              <div className="d-flex align-items-center mb-3">
+                <label className="form-label d-inline m-0 me-2">Akun</label>
+                <select className="form-select border-0 border-bottom bg-transparent shadow-none" aria-label="Default select example">
+                  <option selected />
+                  <option value="1">Dompet</option>
+                  <option value="2">Rekening</option>
+                </select>
+              </div>
+              <div className="d-flex align-items-center mb-3">
+                <label className="form-label d-inline m-0 me-2">Catatan</label>
+                <input name="jumlah" type="text" className="form-control border-0 border-bottom bg-transparent shadow-none" />
+              </div>
+              <div className="text-center">
+                <button type="button" className="btn btn-primary rounded-pill px-5 py-2">Simpan</button>
+              </div>
+            </form>
+          </div>
+
+          <div className="tab-pane fade" id="pengeluaran-tab-pane" role="tabpanel" aria-labelledby="pengeluaran-tab" tabIndex="0">
+            <form className="my-4 text-gray-50">
+              <div className="d-flex align-items-center mb-3">
+                <label className="form-label d-inline m-0 me-2">Tanggal</label>
+                <input name="tanggal" type="date" className="form-control border-0 border-bottom bg-transparent shadow-none" />
+              </div>
+              <div className="d-flex align-items-center mb-3">
+                <label className="form-label d-inline m-0 me-2">Jumlah</label>
+                <input name="jumlah" type="text" className="form-control border-0 border-bottom bg-transparent shadow-none" />
+              </div>
+              <div className="d-flex align-items-center mb-3">
+                <label className="form-label d-inline m-0 me-2">Kategori</label>
+                <select className="form-select border-0 border-bottom bg-transparent shadow-none" aria-label="Default select example">
+                  <option selected />
+                  <option value="1">One</option>
+                  <option value="2">Two</option>
+                  <option value="3">Three</option>
+                </select>
+              </div>
+              <div className="d-flex align-items-center mb-3">
+                <label className="form-label d-inline m-0 me-2">Akun</label>
+                <select className="form-select border-0 border-bottom bg-transparent shadow-none" aria-label="Default select example">
+                  <option selected />
+                  <option value="1">Dompet</option>
+                  <option value="2">Rekening</option>
+                </select>
+              </div>
+              <div className="d-flex align-items-center mb-3">
+                <label className="form-label d-inline m-0 me-2">Catatan</label>
+                <input name="jumlah" type="text" className="form-control border-0 border-bottom bg-transparent shadow-none" />
+              </div>
+              <div className="text-center">
+                <button type="button" className="btn btn-primary rounded-pill px-5 py-2">Simpan</button>
+              </div>
+            </form>
+          </div>
         </div>
-        <div className="tab-pane fade" id="pengeluaran-tab-pane" role="tabpanel" aria-labelledby="pengeluaran-tab" tabIndex="0">
-          <h3 className="py-3 text-center">Tab Pengeluaran</h3>
-        </div>
-      </div>
+      </main>
     </>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
       <h1 className="display-5 m-0 pt-3">Rp100.000</h1>
       <p className="text-gray-50">Jumlah saldo</p>
 
-      <div className="bg-white rounded p-3 mb-4">
+      <div className="bg-white rounded-5 p-3 mb-4">
         <div className="form-check form-check-reverse form-switch">
           <label className="form-check-label float-start fw-bold" htmlFor="notificationCheck">
             <img src={Bell} alt="" className="me-2" />
@@ -23,7 +23,7 @@ function Home() {
         </div>
       </div>
 
-      <div className="bg-white rounded p-3 mb-4" style={{ height: '150px' }}>
+      <div className="bg-white rounded-5 p-3 mb-4" style={{ height: '150px' }}>
         <header className="d-flex justify-content-between border-bottom">
           <p className="fw-bold">Keuangan Saya</p>
           <Link to="/" className="text-secondary text-decoration-none">Lihat semua</Link>
@@ -34,7 +34,7 @@ function Home() {
         <p className="fw-bold text-gray-50 m-0">Transaksi Terkini</p>
         <Link to="/" className="text-secondary text-decoration-none">Lihat semua</Link>
       </header>
-      <div className="bg-white rounded p-3 mb-4" style={{ height: '150px' }} />
+      <div className="bg-white rounded-5 p-3 mb-4" style={{ height: '150px' }} />
     </>
   );
 }
