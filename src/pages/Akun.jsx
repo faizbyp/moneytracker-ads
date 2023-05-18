a27c4d7eaae35f7c26e8cca4f2b450b7ed9c20f3
faizbyp <faizbyp@gmail.com>
2023-05-18 15:42:22 +0700

    fix account page

--- src/pages/Akun.jsx
+++ src/pages/Akun.jsx
@@ -1,9 +1,12 @@
 import { useNavigate } from 'react-router-dom';
+import { useContext } from 'react';
 import ProfilePic from '../assets/icons/ProfilePic.svg';
 import { deleteCookies } from '../utils/cookiesManager';
 import { accessToken, userId, userName } from '../utils/constant';
+import { UserContext } from '../components/UserProvider';
 
 function Akun() {
+  const user = useContext(UserContext);
   const navigate = useNavigate();
 
   return (
@@ -14,8 +17,8 @@ function Akun() {
 
       <main className="d-flex flex-column justify-content-center align-items-center" style={{ height: '24rem' }}>
         <img src={ProfilePic} alt="" />
-        <h2>Nama</h2>
-        <p>email@gmail.com</p>
+        <h2>{user.name}</h2>
+        <p>{`User ID: ${user.id}`}</p>
         <br />
         <button
           type="button"
